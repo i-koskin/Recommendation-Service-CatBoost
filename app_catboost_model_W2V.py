a890795b9463c8066a29637768c5e625034d9b2d
Igor Koskin <ikoskin1@gmail.com>
2025-02-19 13:25:59 +0300

    Add comments app_catboost_model_W2V.py

--- app_catboost_model_W2V.py
+++ app_catboost_model_W2V.py
@@ -1,43 +1,46 @@
-from fastapi import FastAPI, Depends, HTTPException
+ffrom fastapi import FastAPI
 import pandas as pd
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine
 import os
 from catboost import CatBoostClassifier
 from typing import List
-from datetime import time, datetime
+from datetime import datetime
 from pydantic import BaseModel
 from sqlalchemy.orm import sessionmaker
 
-
+# Создаем экземпляр FastAPI
 app = FastAPI()
 
+# Создаем подключение к базе данных PostgreSQL
 engine = create_engine(
     "postgresql://robot-startml-ro:pheiph0hahj1Vaif@"
     "postgres.lab.karpov.courses:6432/startml"
 )
+
+# Создаем локальную сессию для работы с базой данных
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-
 def get_db():
-    
+    # Функция для получения локальной сессии базы данных
     with SessionLocal() as db:
         return db
 
-
+# Модель для представления данных поста
 class PostGet(BaseModel):
     id: int
     text: str
     topic: str
 
-
 def get_model_path(path: str) -> str:
-    if os.environ.get("IS_LMS") == "1":  # проверяем где выполняется код в лмс, или локально. Немного магии
+    # Функция для получения пути к модели в зависимости от окружения
+    if os.environ.get("IS_LMS") == "1":  # Проверяем, выполняется ли код в LMS или локально
         MODEL_PATH = '/workdir/user_input/model'
     else:
         MODEL_PATH = path
     return MODEL_PATH
 
 def load_models():
+    # Функция для загрузки модели CatBoost
     model_path = get_model_path("/Users/user/Downloads/HW_/catboost_model_W2V")
     loaded_model = CatBoostClassifier()
     loaded_model.load_model(model_path)
@@ -45,7 +48,8 @@ def load_models():
 
 
 def batch_load_sql(query: str) -> pd.DataFrame:
-    CHUNKSIZE = 200000
+    # Функция для загрузки данных из SQL базы по частям
+    CHUNKSIZE = 200000  # Размер чанка для загрузки данных
     
     conn = engine.connect().execution_options(stream_results=True)
     chunks = []
@@ -56,22 +60,27 @@ def batch_load_sql(query: str) -> pd.DataFrame:
 
 
 def load_posts_features() -> pd.DataFrame:
+    # Функция для загрузки характеристик постов
     return batch_load_sql('SELECT * FROM i_koskin_posts_features_lesson_25')
 
 def load_features() -> pd.DataFrame:
+    # Функция для загрузки характеристик пользователей
     return batch_load_sql('SELECT * FROM i_koskin_users_features_lesson_25')
 
 
 def load_post_text() -> pd.DataFrame:
+    # Функция для загрузки текстов постов
     return batch_load_sql('SELECT * FROM public.post_text_df')
 
 def load_liked_posts() -> pd.DataFrame:
+    # Функция для загрузки постов, которые пользователи лайкнули
     return batch_load_sql("""
                           SELECT distinct post_id, user_id
                           FROM public.feed_data
                           WHERE action='like'
                           """)
 
+# Загрузка модели и данных
 model = load_models()
 df_user = load_features()
 df_post = load_posts_features()
@@ -79,7 +88,7 @@ post_table = load_post_text()
 liked_posts = load_liked_posts()
 
 def get_recommended_feed(id: int, time: datetime, limit: int):
-
+    # Функция для получения рекомендованного фида для пользователя по его ID
     # Получение фич пользователя по его ID
     user_features = df_user.loc[df_user['user_id'] == id]
     user_features = user_features.drop(['user_id'], axis=1)
@@ -132,7 +141,7 @@ def get_recommended_feed(id: int, time: datetime, limit: int):
         }) for i in recommended_posts
     ]
 
-
+# Эндпоинт для получения рекомендованных постов
 @app.get('/post/recommendations/', response_model=List[PostGet])
 def recommended_posts(id: int, time: datetime, limit: int=10) -> List[PostGet]:
     return get_recommended_feed(id, time, limit)    
