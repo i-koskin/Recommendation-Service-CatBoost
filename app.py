065c35b5068dd1183459fa487a1852a66fb03878
Igor Koskin <ikoskin1@gmail.com>
2025-02-19 14:04:10 +0300

    Add comments app.py

--- app.py
+++ app.py
@@ -11,41 +11,43 @@ import hashlib
 import logging
 from loguru import logger
 
-
+# Создаем экземпляр FastAPI
 app = FastAPI()
 
 # Константы для разделения на A/B группы
 SALT = "salt_value"
 CONTROL_PERCENT = 0.5  # 50/50 разделение
 
-
+# Создаем подключение к базе данных PostgreSQL
 engine = create_engine(
     "postgresql://robot-startml-ro:pheiph0hahj1Vaif@"
     "postgres.lab.karpov.courses:6432/startml"
 )
+# Создаем локальную сессию для работы с базой данных
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
 def get_db():
-    
-    with SessionLocal() as db:
+    # Функция для получения локальной сессии базы данных
+    with SessionLocal() as db: # Используем контекстный менеджер для автоматического закрытия сессии
         return db
 
-
+# Модель для представления постов
 class PostGet(BaseModel):
     id: int
     text: str
     topic: str
 
     class Config:
-        orm_mode = True
+        orm_mode = True # Включаем режим ORM для поддержки работы с SQLAlchemy
 
+# Модель для ответа с рекомендациями
 class Response(BaseModel):
     exp_group: str
     recommendations: List[PostGet]
 
 
-# Загрузка модели с поддержкой выбора тестовой и контрольной
+# Функция для получения пути к модели в зависимости от окружения
 def get_model_path(model_name: str) -> str:
     if os.environ.get("IS_LMS") == "1":  # проверяем где выполняется код в лмс, или локально. Немного магии
         model_paths = {
@@ -58,31 +60,30 @@ def get_model_path(model_name: str) -> str:
             'test': '/Users/user/Downloads/HW_/mcatboost_model_PCA'  # локальный путь к тестовой модели
         }
 
-    # Проверяем, существует ли указанный путь
     if model_name not in model_paths:
         raise ValueError(f"Unknown model: {model_name}")
 
     return model_paths[model_name]
 
-
+# Функция для загрузки модели
 def load_models(model_name: str):
     model_path = get_model_path(model_name)
     loaded_model = CatBoostClassifier()
     loaded_model.load_model(model_path)
     return loaded_model
 
-
+# Функция для пакетной загрузки данных из SQL в DataFrame
 def batch_load_sql(query: str) -> pd.DataFrame:
-    CHUNKSIZE = 200000
-    
-    conn = engine.connect().execution_options(stream_results=True)
+    CHUNKSIZE = 200000 # Размер чанка для загрузки данных
+
+    conn = engine.connect().execution_options(stream_results=True) # Устанавливаем соединение с БД
     chunks = []
     for chunk_dataframe in pd.read_sql(query, conn, chunksize=CHUNKSIZE):
         chunks.append(chunk_dataframe)
     conn.close()
     return pd.concat(chunks, ignore_index=True)
 
-
+# Функция для загрузки признаков постов в зависимости от модели
 def load_posts_features(model_name: str) -> pd.DataFrame:
     if model_name == 'control':
         return batch_load_sql('SELECT * FROM i_koskin_posts_features_lesson_22')
@@ -91,14 +92,15 @@ def load_posts_features(model_name: str) -> pd.DataFrame:
     else:
         raise ValueError(f"Unknown model: {model_name}")
     
-
+# Функция для загрузки признаков пользователей
 def load_users_features() -> pd.DataFrame:
     return batch_load_sql('SELECT * FROM i_koskin_users_features_lesson_22')
 
-
+# Функция для загрузки текстов постов
 def load_post_text() -> pd.DataFrame:
     return batch_load_sql('SELECT * FROM public.post_text_df')
 
+# Функция для загрузки постов, которые пользователи лайкнули
 def load_liked_posts() -> pd.DataFrame:
     return batch_load_sql("""
                           SELECT distinct post_id, user_id
@@ -106,8 +108,9 @@ def load_liked_posts() -> pd.DataFrame:
                           WHERE action='like'
                           """)
 
-model_control = load_models('control')  # загружаем контрольную модель
-model_test = load_models('test')  # загружаем тестовую модель
+# Загружаем модели и данные
+model_control = load_models('control')  # Загружаем контрольную модель
+model_test = load_models('test')  # Загружаем тестовую модель
 df_user = load_users_features()
 df_post_control = load_posts_features('control')
 df_post_test = load_posts_features('test')
@@ -121,55 +124,58 @@ def get_exp_group(user_id: int) -> str:
 
 # Функции для рекомендаций, привязанные к моделям
 def recommend_with_control_model(id: int, time: datetime, limit: int, exp_group: str) -> List[PostGet]:
+    # Функция для получения рекомендаций с использованием контрольной модели
     return get_recommended_feed(model_control, id, time, limit, exp_group)
 
 def recommend_with_test_model(id: int, time: datetime, limit: int, exp_group: str) -> List[PostGet]:
+    # Функция для получения рекомендаций с использованием тестовой модели
     return get_recommended_feed(model_test, id, time, limit, exp_group)
 
+def extract_user_features(user_id: int) -> pd.Series:
+    # Функция для получения характеристик пользователя
+    logger.info(f'Извлечение характеристик для user_id: {user_id}')
+    user_features = df_user.loc[df_user['user_id'] == user_id]
+    if user_features.empty:
+        raise ValueError(f'Пользователь с ID {user_id} не найден.')
+    return user_features.drop(['user_id'], axis=1).iloc[0]
 
-def get_recommended_feed(model, id: int, time: datetime, limit: int, exp_group: str):
-
-    # Получение фич пользователя по его ID
-    logger.info(f'user_id: {id}')
-    logger.info('reading user features')
-    user_features = df_user.loc[df_user['user_id'] == id]
-    user_features = user_features.drop(['user_id'], axis=1)
-
-    # Загрузка фич по постам
-    logger.info('reading posts features')
-    if exp_group == 'control':
-        posts_features = df_post_control.copy()
-    elif exp_group == 'test':
-        posts_features = df_post_test.copy()
-    else:
-        raise ValueError('Unknown group')
+def add_time_features(df: pd.DataFrame, current_time: datetime) -> pd.DataFrame:
+    # Функция для добавления временных характеристик
+    logger.info('Добавление временных характеристик')
     
+    # Преобразуем текущее время в datetime
+    current_time = pd.to_datetime(current_time)
     
-    # Объединение фич
-    logger.info('zipping features')
-    add_user_features = dict(zip(user_features.columns, user_features.values[0]))
-    logger.info('assigning features')
-    user_posts_features = posts_features.assign(**add_user_features)
-    user_posts_features = user_posts_features.reset_index(drop=True)
+    # Добавляем временные характеристики
+    df['hour'] = pd.to_datetime(current_time).hour
+    df['weekday'] = pd.to_datetime(current_time).day_of_week # 0 - понедельник, 6 - воскресенье
     
-    # Добавление фич о текущей дате рекомендаций
-    logger.info('add time info')
-    user_posts_features['hour'] = pd.to_datetime(time).hour
-    user_posts_features['weekday'] = pd.to_datetime(time).day_of_week
-    user_posts_features['time_of_day'] = pd.cut(
-        user_posts_features['hour'],
-        bins=[0, 6, 12, 18, 24],
-        labels=['night', 'morning', 'afternoon', 'evening'],
-        right=False
-        )
-    user_posts_features['day_of_week'] = pd.cut(
-        user_posts_features['weekday'],
-        bins=[-1, 4, 6],
-        labels=['weekday', 'weekend']
-        )
+    # Определяем время суток
+    df['time_of_day'] = pd.cut(df['hour'],
+                               bins=[0, 6, 12, 18, 24],
+                               labels=['ночь', 'утро', 'день', 'вечер'],
+                               right=False)
+    
+    # Определяем тип дня
+    df['day_of_week'] = pd.cut(df['weekday'],
+                               bins=[-1, 4, 6],
+                               labels=['будний', 'выходной'])
+    return df.drop(['hour', 'weekday'], axis=1)
 
-    user_posts_features = user_posts_features.drop(['hour', 'weekday'], axis=1)
-
+def get_recommended_feed(model, user_id: int, current_time: datetime, limit: int, exp_group: str):
+    # Функция для получения списка рекоммендованных постов
+    # Получаем характеристики пользователя
+    user_features = extract_user_features(user_id)
+    
+    # Получаем DataFrame с постами (df_post_control или df_post_test)
+    posts = df_post_control if user_is_in_control_group(user_id) else df_post_test
+    
+    # Добавляем временные характеристики к DataFrame постов
+    posts_with_time_features = add_time_features(posts, current_time)
+    
+    # Объединяем характеристики пользователя с постами
+    user_posts_features = user_features.append(posts_with_time_features, ignore_index=True)
+    
     # Закрепление порядка колонок
     if exp_group == 'control':
         user_posts_features = user_posts_features[['post_id', 'time_of_day', 'day_of_week', 'topic',
@@ -202,18 +208,18 @@ def get_recommended_feed(model, id: int, time: datetime, limit: int, exp_group: 
         raise ValueError('Unknown group')
     
 
-    # Формировка вероятности лайкнуть пост для всех постов
+    # Формируем вероятности лайкнуть пост для всех постов
     logger.info('predicting')
     predicts = model.predict_proba(user_posts_features)[:, 1]
     user_posts_features['predicts'] = predicts
 
-    # Удаление постов, лайкнутых пользователем
+    # Удаляем посты, лайкнутые пользователем
     logger.info('deleting liked posts')
     like_posts = liked_posts
     like_posts = list(like_posts[like_posts['user_id'] == id])
     filtered_ = user_posts_features[~user_posts_features.post_id.isin(like_posts)]
 
-    # Формирование списка постов для рекомендий
+    # Формируем список рекомендованных постов
     recommended_posts = filtered_.sort_values('predicts')[-limit:].post_id
 
     return [
