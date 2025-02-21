f9d4381d226bcaf66e1cbaf0dfa3211bf76928f7
Igor Koskin <ikoskin1@gmail.com>
2025-02-21 12:11:19 +0300

    Update app_catboost_model_PCA.py

--- app_catboost_model_PCA.py
+++ app_catboost_model_PCA.py
@@ -36,6 +36,9 @@ class PostGet(BaseModel):
     text: str
     topic: str
 
+    class Config:
+        from_attributes = True # Включаем режим ORM для поддержки работы с SQLAlchemy
+
 def get_model_path(path: str) -> str:
     # Функция для получения пути к модели
     MODEL_PATH = path
@@ -43,7 +46,7 @@ def get_model_path(path: str) -> str:
 
 def load_models():
     # Функция для загрузки модели CatBoost
-    model_path = get_model_path("./catboost_model_PCA")
+    model_path = get_model_path("catboost_model_PCA")
     loaded_model = CatBoostClassifier()
     loaded_model.load_model(model_path)
     return loaded_model
@@ -86,70 +89,58 @@ df_post = load_posts_features()
 post_table = load_post_text()
 liked_posts = load_liked_posts()
 
-def extract_user_features(user_id: int) -> pd.Series:
-    # Функция для получения характеристик пользователя
-    logger.info(f'Извлечение характеристик для user_id: {user_id}')
-    user_features = df_user.loc[df_user['user_id'] == user_id]
-    if user_features.empty:
-        raise ValueError(f'Пользователь с ID {user_id} не найден.')
-    return user_features.drop(['user_id'], axis=1).iloc[0]
-
-def add_time_features(df: pd.DataFrame, current_time: datetime) -> pd.DataFrame:
-    # Функция для добавления временных характеристик
-    logger.info('Добавление временных характеристик')
-    
-    # Преобразуем текущее время в datetime
-    current_time = pd.to_datetime(current_time)
-    
-    # Добавляем временные характеристики
-    df['hour'] = pd.to_datetime(current_time).hour
-    df['weekday'] = pd.to_datetime(current_time).day_of_week # 0 - понедельник, 6 - воскресенье
-    
-    # Определяем время суток
-    df['time_of_day'] = pd.cut(df['hour'],
-                               bins=[0, 6, 12, 18, 24],
-                               labels=['ночь', 'утро', 'день', 'вечер'],
-                               right=False)
-    
-    # Определяем тип дня
-    df['day_of_week'] = pd.cut(df['weekday'],
-                               bins=[-1, 4, 6],
-                               labels=['будний', 'выходной'])
-    return df.drop(['hour', 'weekday'], axis=1)
-
-def get_recommended_feed(user_id: int, current_time: datetime, limit: int):
+def get_recommended_feed(id: int, time: datetime, limit: int = 10):
     # Функция для получения списка рекоммендованных постов
-    # Получаем характеристики пользователя
-    user_features = extract_user_features(user_id)
+    # Получение фич пользователя по его ID
+    user_features = df_user.loc[df_user['user_id'] == id]
+    user_features = user_features.drop(['user_id'], axis=1)
+
+    # Загрузка фич по постам
+    posts_features = df_post.copy()
     
-    # Получаем DataFrame с постами
-    posts = df_post.copy()
+    # Объединение фич
+    add_user_features = dict(zip(user_features.columns, user_features.values[0]))
+    user_posts_features = posts_features.assign(**add_user_features)
+    user_posts_features = user_posts_features.reset_index(drop=True)
     
-    # Добавляем временные характеристики к DataFrame постов
-    posts_with_time_features = add_time_features(posts, current_time)
-    
-    # Объединяем характеристики пользователя с постами
-    user_posts_features = user_features.append(posts_with_time_features, ignore_index=True)
+    # Добавление фич о текущей дате рекомендаций
+    time = datetime.now()
+    time = time.strftime("%Y-%m-%dT%H:%M:%S")
+    user_posts_features['hour'] = pd.to_datetime(time).hour
+    user_posts_features['weekday'] = pd.to_datetime(time).day_of_week
+    user_posts_features['time_of_day'] = pd.cut(
+        user_posts_features['hour'],
+        bins=[0, 6, 12, 18, 24],
+        labels=['night', 'morning', 'afternoon', 'evening'],
+        right=False
+        )
+    user_posts_features['day_of_week'] = pd.cut(
+        user_posts_features['weekday'],
+        bins=[-1, 4, 6],
+        labels=['weekday', 'weekend']
+        )
+
+    user_posts_features = user_posts_features.drop(['hour', 'weekday'], axis=1)
 
     # Закрепление порядка колонок
     user_posts_features = user_posts_features[['post_id', 'time_of_day', 'day_of_week', 'topic',
-                                               'pca_1', 'pca_2', 'gender', 'city', 'exp_group',
+                                               'pca_1', 'pca_2', 'gender', 'city','exp_group',
                                                'os', 'source', 'age_group']]
 
-    # Формируем вероятности лайкнуть пост для всех постов
+    # Формировка вероятности лайкнуть пост для всех постов
     predicts = model.predict_proba(user_posts_features)[:, 1]
     user_posts_features['predicts'] = predicts
 
-    # Удаляем посты, лайкнутые пользователем
+    # Удаление постов, лайкнутых пользователем
     like_posts = liked_posts
-    like_posts = list(like_posts[like_posts['user_id'] == id]['post_id'])
+    like_posts = list(like_posts[like_posts['user_id'] == id])
     filtered_ = user_posts_features[~user_posts_features.post_id.isin(like_posts)]
 
-    # Формируем список рекомендованных постов
+    # Формирование списка постов для рекомендий
     recommended_posts = filtered_.sort_values('predicts')[-limit:].post_id
 
     return [
-        PostGet({
+        PostGet(**{
             'id': i,
             'text': post_table[post_table['post_id'] == i].text.values[0],
             'topic': post_table[post_table['post_id'] == i].topic.values[0]
@@ -158,5 +149,11 @@ def get_recommended_feed(user_id: int, current_time: datetime, limit: int):
 
 # Эндпоинт для получения рекомендованных постов
 @app.get('/post/recommendations/', response_model=List[PostGet])
-def recommended_posts(id: int, time: datetime, limit: int = 10) -> List[PostGet]:
-    return get_recommended_feed(id, time, limit) 
+def recommended_posts(id: int, limit: int = 10) -> List[PostGet]:
+    return get_recommended_feed(id, limit)
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000) 
+
